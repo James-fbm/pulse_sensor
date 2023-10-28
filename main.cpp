508fe79094c384975b6c509d715935771da54379
james <james@1433170308@qq.com>
2023-10-28 19:04:34 +0100

    batch write

--- main.cpp
+++ main.cpp
@@ -35,9 +35,18 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
 
     InfluxDB ix;
-    for (unsigned long i = 1697894400007 ;; ++i) {
-        ix.sendData(QString("weather,location=us-midwest temperature=82 ") + QString::number(i, 10));
-        qDebug() << "i = " << i - 1697894400006;
+    for (unsigned long i = 1698894500007 ;; i += 512) {
+        QString buf_data("");
+        for (unsigned j = 0 ; j < 512 ; ++j) {
+            // buf_data += "w_512,location=us-midwest temperature=82 ";
+            buf_data += "s_512 value=82.3 ";
+            buf_data += QString::number(i+j, 10);
+            buf_data += '\n';
+        }
+        ix.sendData(buf_data);
+        // qDebug() << buf_data;
+        qDebug() << buf_data.size();
+        qDebug() << "i = " << i - 1698894500006;
     }
     return a.exec();
 }
